cc69453dafbd8f9091dcc0cb9557da61774efe51
wish <hjq690264423@vip.qq.com>
2019-08-02 15:11:33 +0800

    init my project

---
+++ src/main.js
@@ -0,0 +1,16 @@
+//入口文件
+import Vue from 'vue'
+import app from './App.vue'
+
+//按需导入mint-ui组件
+import {Header} from 'mint-ui'
+Vue.component(Header.name,Header)
+
+//导入mui样式
+import './lib/mui/css/mui.min.css'
+
+
+var vm = new Vue({
+    el: '#app',
+    render: c => c(app)
+})
